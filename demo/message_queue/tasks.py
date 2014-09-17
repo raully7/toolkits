02e5629240c3559362b7851367bd3afdc7670b72
Yang Liu <raully7@gmail.com>
2014-09-17 16:07:05 +0800

    add log for tasks

--- demo/message_queue/tasks.py
+++ demo/message_queue/tasks.py
@@ -5,7 +5,14 @@ from __future__ import absolute_import
 from message_queue.celery import app
 from random import randint
 
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
+
 
 @app.task
 def random(x, y):
+
+    logger.info('Random in range {0} + {1}'.format(x, y))
+
     return randint(x, y)
